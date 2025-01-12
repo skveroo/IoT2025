6e756c82729d368caefea0a10b1fb288d760ed13
skveroo <maciek90908@gmail.com>
2025-01-12 23:29:33 +0100

    Twin properties and emergency stop
    Device twin now has desired production rate and reported production rate and error status. It is possible to directly invoke emergency stop method.

--- AzureAgent/AzureAgent/FeatureSelector.cs
+++ AzureAgent/AzureAgent/FeatureSelector.cs
@@ -8,12 +8,24 @@ using Opc.UaFx.Client;
 using System;
 using System.Text;
 using Microsoft.Azure.Devices.Shared;
+using Newtonsoft.Json.Linq;
+using System.Net.Sockets;
+using Opc.Ua;
+using System.Runtime.Intrinsics.Arm;
+using ServiceSDK.Lib;
+enum Errors
+{
+    EmergencyStop = 1,
+    PowerFailure = 2,
+    SensorFailue = 4,
+    Unknown = 8
+}
 
 namespace ServiceSdkDemo.Console
 {
-    
     internal static class FeatureSelector
     {
+        public static bool twinInitialized = false;
         public static void PrintMenu()
         {
             System.Console.WriteLine(@"
@@ -25,8 +37,14 @@ namespace ServiceSdkDemo.Console
     0 - Exit");
         }
 
-        public static async Task Execute(int feature, ServiceSDK.Lib.IoTHubManager manager, string deviceId,int id, DeviceClient deviceClient)
+        public static async Task Execute(int feature, ServiceSDK.Lib.IoTHubManager manager, string deviceId, int id, DeviceClient deviceClient)
         {
+            var client = new OpcClient("opc.tcp://localhost:4840/");
+            client.Connect();
+
+            
+           
+           
             switch (feature)
             {
                 case 1:
@@ -53,58 +71,21 @@ namespace ServiceSdkDemo.Console
                     break;
                 case 3:
                     {
-                        System.Console.WriteLine("\nType property name (confirm with enter):");
-                        string propertyName = System.Console.ReadLine() ?? string.Empty;
-                        var random = new Random();
-                        await manager.UpdateDesiredTwin(deviceId, propertyName, random.Next());
+                        await updateTwinData(deviceClient, client, id);
+                        await manager.SetDesiredProductionRate(50, deviceId);
                     }
                     break;
                 case 4:
                     {
-                        using (var client = new OpcClient("opc.tcp://localhost:4840/"))
-                        {
-                            string deviceNumber= $"Device {id}";
-                            
-                            client.Connect();
-                            System.Console.WriteLine();
-                            OpcReadNode[] nodeAttributes = new OpcReadNode[] {
-                            new OpcReadNode($"ns=2;s={deviceNumber}/ProductionStatus", OpcAttribute.DisplayName),
-                            new OpcReadNode($"ns=2;s={deviceNumber}/ProductionRate", OpcAttribute.DisplayName),
-                            new OpcReadNode($"ns=2;s={deviceNumber}/WorkorderId", OpcAttribute.DisplayName),
-                            new OpcReadNode($"ns=2;s={deviceNumber}/Temperature", OpcAttribute.DisplayName),
-                            new OpcReadNode($"ns=2;s={deviceNumber}/GoodCount", OpcAttribute.DisplayName),
-                            new OpcReadNode($"ns=2;s={deviceNumber}/BadCount", OpcAttribute.DisplayName),
-                            new OpcReadNode($"ns=2;s={deviceNumber}/DeviceError", OpcAttribute.DisplayName),
-};
-                            OpcReadNode[] nodeValues = new OpcReadNode[] {
-                            new OpcReadNode($"ns=2;s={deviceNumber}/ProductionStatus"),
-                            new OpcReadNode($"ns=2;s={deviceNumber}/ProductionRate"),
-                            new OpcReadNode($"ns=2;s={deviceNumber}/WorkorderId"),
-                            new OpcReadNode($"ns=2;s={deviceNumber}/Temperature"),
-                            new OpcReadNode($"ns=2;s={deviceNumber}/GoodCount"),
-                            new OpcReadNode($"ns=2;s={deviceNumber}/BadCount"),
-                            new OpcReadNode($"ns=2;s={deviceNumber}/DeviceError"),
-};
-                            var data = new System.Dynamic.ExpandoObject() as dynamic;
-                            IEnumerable<OpcValue> attributeResults = client.ReadNodes(nodeAttributes);
-                            IEnumerable<OpcValue> valueResults = client.ReadNodes(nodeValues);
-                            var combinedResults = attributeResults.Zip(valueResults, (attribute, value) => new { Attribute = attribute.Value, Value = value.Value });
-                            foreach (var item in combinedResults)
-                            {
-                                ((IDictionary<string, object>)data)[item.Attribute.ToString()] = item.Value;
-                            }
+                        displayTelemetry(id, client);
+                        var data = new System.Dynamic.ExpandoObject() as dynamic;
+                        await SendTelemetryData(deviceClient, data);
 
-                            foreach (var prop in (IDictionary<string, object>)data)
-                            {
-                                System.Console.WriteLine($"{prop.Key}: {prop.Value}");
-                            }
-                            await SendTelemetryData(deviceClient, data);
-                        }
                     }
                     break;
                 case 5:
                     {
-                        
+
                         System.Console.WriteLine("\nDisconnecting...");
                         await Task.Delay(2000);
                         System.Console.Clear();
@@ -115,6 +96,9 @@ namespace ServiceSdkDemo.Console
                 default:
                     break;
             }
+
+
+
         }
 
         public static async Task SendTelemetryData(DeviceClient client, dynamic data)
@@ -128,6 +112,7 @@ namespace ServiceSdkDemo.Console
             };
             await client.SendEventAsync(eventMessage);
         }
+
 
         private static void RestartProgram()
         {
@@ -168,6 +153,128 @@ namespace ServiceSdkDemo.Console
             var keyPressed = System.Console.ReadKey();
             var isParsed = int.TryParse(keyPressed.KeyChar.ToString(), out int result);
             return isParsed ? result : -1;
+        }
+
+        public static async Task updateTwinData(DeviceClient deviceClient, OpcClient client, int id)
+        {
+            var twin = await deviceClient.GetTwinAsync();
+            if (!twinInitialized)
+            {
+                await updateTwinError(errorDetection(client, id), deviceClient);
+                await updateTwinProductionValues(getProductionRate(client, id), deviceClient);
+                twinInitialized = true;
+            }
+        }
+        public static async Task updateTwinError(string deviceErrors, DeviceClient deviceClient)
+        {
+            var reportedDeviceTwin = new TwinCollection();
+            reportedDeviceTwin["deviceErrors"] = deviceErrors;
+            reportedDeviceTwin["lastErrorDate"] = DateTime.Today;
+            await deviceClient.UpdateReportedPropertiesAsync(reportedDeviceTwin);
+        }
+        public static async Task updateTwinProductionValues(int reportedProduction, DeviceClient deviceClient)
+        {
+            var reportedProperties = new TwinCollection();
+            reportedProperties["reportedProduction"] = reportedProduction;
+
+            await deviceClient.UpdateReportedPropertiesAsync(reportedProperties);
+        }
+
+        private static string errorDetection(OpcClient client, int id)
+        {
+            string errors = string.Empty;
+
+            int deviceErrors = (int)client.ReadNode($"ns=2;s=Device {id}/DeviceError").Value;
+
+            var errorMessages = new List<string>();
+
+            if ((deviceErrors & (int)Errors.Unknown) != 0)
+                errorMessages.Add("Unknown");
+
+            if ((deviceErrors & (int)Errors.SensorFailue) != 0)
+                errorMessages.Add("SensorFailure");
+
+            if ((deviceErrors & (int)Errors.PowerFailure) != 0)
+                errorMessages.Add("PowerFailure");
+
+            if ((deviceErrors & (int)Errors.EmergencyStop) != 0)
+                errorMessages.Add("Emergency stop");
+
+            errors = string.Join(", ", errorMessages);
+            return errors;
+
+        }
+        private static int getProductionRate(OpcClient client, int id)
+        {
+            int productionRate = (int)client.ReadNode($"ns=2;s=Device {id}/ProductionRate").Value;
+            return productionRate;
+        }
+        private static void displayTelemetry(int id, OpcClient client)
+            {
+                            string deviceNumber = $"Device {id}";
+                            System.Console.WriteLine();
+                            OpcReadNode[] nodeAttributes = new OpcReadNode[] {
+                            new OpcReadNode($"ns=2;s={deviceNumber}/ProductionStatus", OpcAttribute.DisplayName),
+                            new OpcReadNode($"ns=2;s={deviceNumber}/ProductionRate", OpcAttribute.DisplayName),
+                            new OpcReadNode($"ns=2;s={deviceNumber}/WorkorderId", OpcAttribute.DisplayName),
+                            new OpcReadNode($"ns=2;s={deviceNumber}/Temperature", OpcAttribute.DisplayName),
+                            new OpcReadNode($"ns=2;s={deviceNumber}/GoodCount", OpcAttribute.DisplayName),
+                            new OpcReadNode($"ns=2;s={deviceNumber}/BadCount", OpcAttribute.DisplayName),
+                            new OpcReadNode($"ns=2;s={deviceNumber}/DeviceError", OpcAttribute.DisplayName),
+};
+                            OpcReadNode[] nodeValues = new OpcReadNode[] {
+                            new OpcReadNode($"ns=2;s={deviceNumber}/ProductionStatus"),
+                            new OpcReadNode($"ns=2;s={deviceNumber}/ProductionRate"),
+                            new OpcReadNode($"ns=2;s={deviceNumber}/WorkorderId"),
+                            new OpcReadNode($"ns=2;s={deviceNumber}/Temperature"),
+                            new OpcReadNode($"ns=2;s={deviceNumber}/GoodCount"),
+                            new OpcReadNode($"ns=2;s={deviceNumber}/BadCount"),
+                            new OpcReadNode($"ns=2;s={deviceNumber}/DeviceError"),
+};
+                            var data = new System.Dynamic.ExpandoObject() as dynamic;
+                            IEnumerable<OpcValue> attributeResults = client.ReadNodes(nodeAttributes);
+                            IEnumerable<OpcValue> valueResults = client.ReadNodes(nodeValues);
+                            var combinedResults = attributeResults.Zip(valueResults, (attribute, value) => new { Attribute = attribute.Value, Value = value.Value });
+                            foreach (var item in combinedResults)
+                            {
+                                ((IDictionary<string, object>) data)[item.Attribute.ToString()] = item.Value;
+                            }
+
+                            foreach (var prop in (IDictionary<string, object>) data)
+                            {
+                                System.Console.WriteLine($"{prop.Key}: {prop.Value}");
+                            }
+            }
+    }
+    public class device
+    {
+        public static DeviceClient deviceClient;
+        public device(DeviceClient deviceClient)
+        {
+            device.deviceClient = deviceClient;
+        }
+        async Task EmergencyStop()
+        {
+            IoTHubManager.EmergencyStop();
+            await (Task.Delay(1000));
+        }
+        private async Task<MethodResponse> EmergencyStopHandler(MethodRequest methodRequest, object userContext)
+        {
+            System.Console.WriteLine($"\tMETHOD EXECUTED: {methodRequest.Name}");
+            await EmergencyStop();
+            return new MethodResponse(0);
+        }
+        private static async Task<MethodResponse> DefaultServiceHandler(MethodRequest methodRequest, object userContext)
+        {
+            System.Console.WriteLine($"\tMETHOD EXECUTED: {methodRequest.Name}");
+            await Task.Delay(1000);
+
+            return new MethodResponse(0);
+        }
+        public async Task InitializeHandlers()
+        {
+            await deviceClient.SetMethodDefaultHandlerAsync(DefaultServiceHandler, null);
+            await deviceClient.SetMethodHandlerAsync("EmergencyStop", EmergencyStopHandler, null);
         }
     }
 }
--- AzureAgent/AzureAgent/Program.cs
+++ AzureAgent/AzureAgent/Program.cs
@@ -6,110 +6,106 @@ using ServiceSDK.Lib;
 using ServiceSdkDemo.Console;
 using Microsoft.Azure.Devices.Client;
 using TransportType = Microsoft.Azure.Devices.Client.TransportType;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
-string filePath = "../../../../settings.txt";
-string opcServerAddress = string.Empty;
-string connectionString = string.Empty;
-Console.WriteLine("Found devices:");
-try
+public class Program
 {
-    string[] lines = File.ReadAllLines(filePath);
-    List<string> devices = new List<string>();
-    List<string> connectionStrings = new List<string>();
-    devices.Add("0");
-    connectionStrings.Add("0");
-
-    int j = 0;
-    for (int i = 0; i < lines.Length; i++)
+    public static string selectedDeviceId;
+    public static async Task Main(string[] args)
     {
-        if (lines[i].Trim() == "server addres:")
-        {
-            if (i + 1 < lines.Length)
-            {
-                opcServerAddress = lines[i + 1].Trim();
-            }
-        }
+        string filePath = "../../../../settings.txt";
+        string opcServerAddress = (File.ReadAllLines($"../../../../settings.txt")[1]);
+        string connectionString = string.Empty;
+        Console.WriteLine("Found devices:");
 
-        if (lines[i].Trim() == "device connection string:")
+        try
         {
-            if (i + 1 < lines.Length)
-            {
-                connectionString = lines[i + 1].Trim();
-            }
-        }
+            string[] lines = File.ReadAllLines(filePath);
+            List<string> devices = new List<string>();
+            List<string> connectionStrings = new List<string>();
+            devices.Add("0");
+            connectionStrings.Add("0");
 
-        if (lines[i].Trim() == "devices:")
-        { 
-            for (int k = i+1; k < lines.Length; k++)
+            int j = 0;
+            for (int i = 0; i < lines.Length; i++)
             {
-                if (lines[k].Trim().StartsWith("HostName="))
+                if (lines[i].Trim() == "device connection string:")
                 {
-                    string line = lines[k].Trim();
-                    j++;
-                    connectionStrings.Add(lines[k]);
-                    string deviceId = ExtractDeviceId(line);
-                    devices.Add(deviceId);
-                    Console.WriteLine($"{j}: {deviceId}");
+                    if (i + 1 < lines.Length)
+                    {
+                        connectionString = lines[i + 1].Trim();
+                    }
+                }
+
+                if (lines[i].Trim() == "devices:")
+                {
+                    for (int k = i + 1; k < lines.Length; k++)
+                    {
+                        if (lines[k].Trim().StartsWith("HostName="))
+                        {
+                            string line = lines[k].Trim();
+                            j++;
+                            connectionStrings.Add(lines[k]);
+                            string deviceId = ExtractDeviceId(line);
+                            devices.Add(deviceId);
+                            Console.WriteLine($"{j}: {deviceId}");
+                        }
+                    }
                 }
             }
+
+            int id = 1;
+            Console.WriteLine($"Selected Device ID: {opcServerAddress}");
+            Console.WriteLine("Choose device number:");
+            id = Convert.ToInt32(Console.ReadLine());
+            selectedDeviceId = devices[id];
+            Console.WriteLine($"Selected Device ID: {selectedDeviceId}");
+
+            using var serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+            using var registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+
+            var manager = new IoTHubManager(serviceClient, registryManager);
+            using var opcClient = new OpcClient($"{opcServerAddress}");
+            opcClient.Connect();
+            Console.WriteLine("Connected to OPC server.");
+
+            string deviceClientConnectionString = connectionStrings[id];
+            using var deviceClient = DeviceClient.CreateFromConnectionString(deviceClientConnectionString, TransportType.Mqtt);
+            await deviceClient.OpenAsync();
+
+            int input;   
+            var device = new device(deviceClient);
+            await device.InitializeHandlers();
+            do
+            {
+                FeatureSelector.PrintMenu();
+                input = FeatureSelector.ReadInput();
+                await FeatureSelector.Execute(input, manager, selectedDeviceId, id, deviceClient);
+            } while (input != 0);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
         }
     }
-    
-        int id = 1;
-        Console.WriteLine("Choose device number:");
-        id = Convert.ToInt32(Console.ReadLine());
-   // Console.WriteLine($"Connection string: {connectionString}");
-   // Console.WriteLine($"DeviceId: {devices[id]}");
-    string selectedDeviceId = (devices[id]);
-        using var serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
-        using var registryManager = RegistryManager.CreateFromConnectionString(connectionString);
 
-        var manager = new IoTHubManager(serviceClient, registryManager);
-        using var opcClient = new OpcClient($"{opcServerAddress}");
-        opcClient.Connect();
-        Console.WriteLine("Connected to OPC server.");
-        int input;
-
-    string deviceClientConnectionString = connectionStrings[id];
-    // Console.WriteLine($"Connection string: {deviceClientConnectionString}");
-    using var deviceClient = DeviceClient.CreateFromConnectionString(deviceClientConnectionString, TransportType.Mqtt);
-    await deviceClient.OpenAsync();
-
-    do
-        {
-            FeatureSelector.PrintMenu();
-            input = FeatureSelector.ReadInput();
-            await FeatureSelector.Execute(input, manager, selectedDeviceId, id, deviceClient);
-        } while (input != 0);
-
-   
-}
-
-
-catch (Exception ex)
-{
-    Console.WriteLine($"An error occurred: {ex.Message}");
-}
-static string ExtractDeviceId(string line)
-{
-    string deviceId = string.Empty;
-
-    // Split the line into parts by semicolon
-    string[] parts = line.Split(';');
-
-    // Look for the part starting with "DeviceId="
-    foreach (string part in parts)
+    static string ExtractDeviceId(string line)
     {
-        if (part.StartsWith("DeviceId="))
+        string deviceId = string.Empty;
+
+        string[] parts = line.Split(';');
+        foreach (string part in parts)
         {
-            deviceId = part.Substring("DeviceId=".Length);
-            break;
+            if (part.StartsWith("DeviceId="))
+            {
+                deviceId = part.Substring("DeviceId=".Length);
+                break;
+            }
         }
+
+        return deviceId;
     }
-
-    return deviceId;
 }
-
-
-
-
